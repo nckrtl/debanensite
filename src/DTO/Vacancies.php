a9f7411661994f5fbf1bc40ada786ec66a895a8f
Nick Retel <nick.retel@gmail.com>
2023-11-08 21:12:34 +0100

    wip

---
+++ src/DTO/Vacancies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nckrtl\DeBanensite\DTO;
+
+use Illuminate\Support\Collection;
+use Saloon\Http\Response;
+
+class Vacancies
+{
+    public static function fromResponse(Response $response): Collection
+    {
+        $data = $response->json();
+
+        return collect([
+            'total' => $data['hydra:totalItems'],
+            'pages' => [
+                'next' => array_key_exists('hydra:next', $data['hydra:view']) ? (int) str_replace('/vacancies?online=1&page=', '', $data['hydra:view']['hydra:next']) : null,
+                'last' => (int) str_replace('/vacancies?online=1&page=', '', $data['hydra:view']['hydra:last']),
+            ],
+            'items' => collect($data['hydra:member'])->map(function ($vacancy) {
+                return Vacancy::defaultFormat($vacancy);
+            }),
+        ]);
+    }
+}
